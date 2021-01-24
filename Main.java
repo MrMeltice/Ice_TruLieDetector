84224d915d363aee8e8c1e132382d6ccd5f46ba2
Meltice <noppanat.amp@student.mahidol.edu>
2021-01-24 12:56:54 +0000

    Start

--- Main.java
+++ Main.java
@@ -4,7 +4,7 @@ class Main {
   public static void clearScreen() {  
     System.out.print("\033[H\033[2J");  
     System.out.flush();  
-  }
+  } 
 
   public static void sleep(int seconds){
     long start = System.currentTimeMillis();
