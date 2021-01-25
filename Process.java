5e17277b9eadf639b79864f3d8665954d7240722
Meltice <noppanat.amp@student.mahidol.edu>
2021-01-25 15:31:47 +0000

    added compare and started on questions

--- Process.java
+++ Process.java
@@ -7,74 +7,114 @@ import java.util.Scanner;
 import java.io.FileNotFoundException;
 
 class Process {
-  static int match;
-
+  static int smellCount, sightCount, touchCount, tasteCount, hearCount;
+  static boolean smell, sight, touch, taste, hear = false;
 
   static ArrayList<String> storyAL = new ArrayList();
   static ArrayList<String> comAL = new ArrayList();
+  
 
+  public void tellMe() {
+    System.out.println("Which of these can you provide more details on?\nPlease note: The more details you are able to provide the more reliable your information will become");
 
+    System.out.println("");
+  }
 
 
   public void removeCommon(String story) {
-    //use story from Main
+    //use story from Main --> ArrayList
 
     //remove things like periods, or commas etc
     String storyList = story.replaceAll("[^a-zA-Z]", " ");
     
     String[] storyArray = storyList.split(" ");
-    
     for (String l : storyArray) {
       storyAL.add(l.toLowerCase());
     }
-    
     while (storyAL.remove("")){
     }
 
-    //import list of common words text file
+
+    //import list of common words text file --> ArrayList
     String commonText = "";
-
     try {
-
       File myFile = new File("comWord.txt");
-      
       Scanner myScan = new Scanner(myFile);
-
       while(myScan.hasNextLine()) {
         commonText += myScan.nextLine() + " ";
       }
       myScan.close();
-    
     } catch (FileNotFoundException e){
       System.out.println("Error Occurred");
       e.printStackTrace();
     }
-
     String[] commonArray = commonText.split(",");
-
     for (String a : commonArray) {
       comAL.add(a.toLowerCase());
     }
 
+
     System.out.println("WordCount storyAL: " + storyAL.size());
 
-    //remove common
-    for (int b = storyAL.size() - 1; b > 0; b--){
-      for (int c = 0; comAL.size() > c; c++){
-        if (storyAL.get(b).equals(comAL.get(c))){
-          storyAL.remove(b);
+    removeCom(storyAL, comAL);
+
+    System.out.println("WordCount storyAL After: " + storyAL.size());
+
+    questApp();
+
+
+  }
+
+  public void questApp() {
+    tellMe();
+    
+    
+
+
+    
+  }
+
+  public void questSmell() {
+    
+    System.out.println(""");
+    Scanner s = new Scanner(System.in);
+    s.nextLine();
+    
+    
+
+
+    
+  }
+
+
+
+
+
+  //Remove common words that could be viewed as duplicates
+  public void removeCom(ArrayList<String> removeFrom, ArrayList<String> usetoRemove) {
+    
+    for (int b = removeFrom.size() - 1; b > 0; b--){
+      for (int c = 0; usetoRemove.size() > c; c++){
+        if (removeFrom.get(b).equals(usetoRemove.get(c))){
+          removeFrom.remove(b);
+        }
+      }
+    }
+  }
+
+  //Compare how many details are dublicates
+  public int compareAL(ArrayList<String> compareTo, ArrayList<String> compareWith) {
+    
+    for (int x = compareTo.size()-1; x > 0; x--){
+      for(int y = 0; y < compareWith.size(); y++){
+        if(compareTo.get(x).equals(compareWith.get(y))){
+          matchResult++;
         }
       }
     }
 
-    System.out.println("WordCount storyAL After: " + storyAL.size());
+    return matchResult;  
     
-
-  
-
-
-
-
   }
 
 }
